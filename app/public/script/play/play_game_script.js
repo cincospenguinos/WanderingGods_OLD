8618c8d97531f9cfd962cf7684c39b88267f55ef
Andre LaFleur <andre.lafleur@utah.edu>
2016-11-15 19:04:06 -0700

    Fix minor play_game_script issues

--- app/public/script/play/play_game_script.js
+++ app/public/script/play/play_game_script.js
@@ -3,5 +3,21 @@
  */
 
 $(document).ready(function(){
-    // TODO: All the usual bullshilogna
+
+    // Sending a command
+    $(document).keydown(function(event){
+        var commandNode = $('#player_command');
+
+        // Enter Key
+        if(event.which === 13) {
+            var command = commandNode.val();
+            commandNode.val('');
+
+            // TODO: Actually send the command
+        }
+    });
+
+    $(document).click(function(){
+        $('#player_command').select();
+    });
 });
